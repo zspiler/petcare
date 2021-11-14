8a423a92f854e3776ddb1338b02ffb866723b83c
zanspiler <zanspiler1@gmail.com>
2021-11-14 13:51:03 +0100

    added vue-router

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
+
+import Register from "./components/Register";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueRouter);
+
+const routes = [{ path: "/register", component: Register }];
+
+const router = new VueRouter({
+	routes,
+	mode: "history", // remove # from url
+});
+
 new Vue({
 	vuetify,
+	router,
 	render: (h) => h(App),
 }).$mount("#app");
